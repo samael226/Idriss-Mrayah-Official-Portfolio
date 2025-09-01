95cebeeab8b9ede0af965e1e8659481db6ed87a6
samael226 <idrissmrayah007@gmail.com>
2025-09-01 11:13:58 +0200

    Integrate Umami analytics script into layout component and remove standalone umami.js file.

--- my-portfolio1/src/app/layout.tsx
+++ my-portfolio1/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-
+import Script from "next/script";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -17,7 +17,15 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <body className={`${inter.className} bg-gray-900 text-white min-h-screen`}>
-        {children}
+        <Script
+          id="umami-script"
+          defer 
+          src="scripts/umami.js"
+          data-website-id={process.env.NEXT_PUBLIC_UMAMI_ID as string}
+          strategy="afterInteractive"
+        >
+          {children}
+        </Script>
       </body>
     </html>
   );
